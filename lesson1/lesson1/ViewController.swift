da9f6a5ec40992937d9e5db6b80b662e5dfbb742
voroshyn <voroshyn.it@gmail.com>
2024-03-26 23:58:23 +0300

    Initial xCode project

---
+++ lesson1/lesson1/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  lesson1
+//
+//  Created by Daniel on 26.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("стирай этот метод и пиши код сюда")
+    }
+}
